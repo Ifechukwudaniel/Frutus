950cc403967f37d76f740cf6482745a8edb418f4
Angry Coding <mailidanielifechukwu@gmail.com>
2021-07-16 02:08:30 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by ifechukwu daniel; on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
